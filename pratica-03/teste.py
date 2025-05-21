e11b8154a3ad1c070e129ad8d8add4a15411e369
Bruno <bruno.marques@escoladanuvem.org>
2025-05-21 11:28:23 -0300

    Prática 03

---
+++ pratica-03/teste.py
@@ -0,0 +1,10 @@
+# Isso é um teste
+
+if True:
+    print("Indentado corretamente")
+    if True:
+        print("Bloco aninhado")
+        if True:
+            print("Outro bloco aninhado")
+else:
+    print("Bloco else")
