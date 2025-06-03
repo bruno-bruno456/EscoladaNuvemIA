c624d13d0532d6cbae37f333cbc2ce98aa954e7b
Bruno <bruno.marques@escoladanuvem.org>
2025-06-02 21:06:32 -0300

    Prática 07

---
+++ pratica-07/ler_escrever_json.py
@@ -0,0 +1,30 @@
+import json
+
+def ler_json(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r', newline='', encoding='utf-8') as arquivo_json:
+            dados = json.load(arquivo_json)
+            print(dados)
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+
+def escrever_json(nome_arquivo, dados):
+    try:
+        with open(nome_arquivo, 'w', newline='', encoding='utf-8') as arquivo_json:
+            json.dump(dados, arquivo_json)
+        print(f"Dados salvos em {nome_arquivo}")
+    except Exception as e:
+        print(f"Erro ao salvar o arquivo: {e}")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")          
+
+dados = {
+    "nome": "João",
+    "idade": 25,
+    "cidade": "Vitória"
+}
+
+if __name__ == "__main__":
+    nome_arquivo = input("Digite o nome do arquivo JSON: ").strip()
+    escrever_json(nome_arquivo, dados)
+    ler_json(nome_arquivo)
